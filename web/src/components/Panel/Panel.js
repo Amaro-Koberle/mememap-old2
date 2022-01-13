7270d37d657487beefd6b55ff97746bcff05b27d
Amaro-Koberle <amaro@amarokoberle.com>
2022-01-13 13:56:33 +0100

    panel now shows node form when edit is included in the url

--- web/src/components/Panel/Panel.js
+++ web/src/components/Panel/Panel.js
@@ -1,11 +1,23 @@
-import { useParams } from '@redwoodjs/router'
+import { useParams, useLocation } from '@redwoodjs/router'
+import EditNodeCell from '../Node/EditNodeCell'
 import NewNode from '../Node/NewNode/NewNode'
 import NodeCell from '../Node/NodeCell/NodeCell'
 
 const Panel = () => {
+  const { pathname } = useLocation()
   const { id } = useParams()
 
-  return <div>{id ? <NodeCell id={id} /> : <NewNode />}</div>
+  return (
+    <div>
+      {pathname.includes('edit') ? (
+        <EditNodeCell id={id} />
+      ) : id ? (
+        <NodeCell id={id} />
+      ) : (
+        <NewNode />
+      )}
+    </div>
+  )
 }
 
 export default Panel
