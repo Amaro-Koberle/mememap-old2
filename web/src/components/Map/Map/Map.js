f50284fa38146f9664c3e84a9eea91ebae7178be
Amaro-Koberle <amaro@amarokoberle.com>
2022-01-13 13:23:11 +0100

    clicking on node in graph viz now navigates to the node url

--- web/src/components/Map/Map/Map.js
+++ web/src/components/Map/Map/Map.js
@@ -1,7 +1,18 @@
 import ForceGraph from 'react-force-graph-3d'
+import { navigate, routes } from '@redwoodjs/router'
+
+const onNodeClick = (node) => {
+  navigate(routes.node({ id: node.id }));
+}
 
 const Map = (data) => {
-  return <ForceGraph backgroundColor="#F5F5F4" graphData={data.graphData} />
+  return (
+    <ForceGraph
+      backgroundColor="#F5F5F4"
+      graphData={data.graphData}
+      onNodeClick={onNodeClick}
+    />
+  )
 }
 
 export default Map
