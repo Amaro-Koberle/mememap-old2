811f370f84f16b5a79b84a61f15455ba8eef4fb5
Amaro-Koberle <amaro@amarokoberle.com>
2022-01-21 11:32:22 +0100

    styling changes to main nav

--- web/src/components/MainNavigation/MainNavigation.js
+++ web/src/components/MainNavigation/MainNavigation.js
@@ -10,50 +10,30 @@ import {
 const MainNavigation = () => {
   return (
     <nav className="m-4 text-xs">
-      <ul className="grid grid-rows-5 grid-cols-1 gap-4 justify-items-center">
+      <ul className="grid grid-rows-5 grid-cols-1 gap-6 justify-items-center">
         <li>
-          <Link
-            className="grid grid-rows-2 justify-items-center gap-2"
-            to={routes.explore()}
-          >
+          <Link className="" to={routes.explore()}>
             <MdOutlineExplore className="text-2xl" />
-            Explore
           </Link>
         </li>
         <li>
-          <Link
-            className="grid grid-rows-2 justify-items-center gap-2"
-            to={routes.explore()}
-          >
+          <Link className="" to={routes.explore()}>
             <MdOutlineAccountBalanceWallet className="text-2xl" />
-            Wallet
           </Link>
         </li>
         <li>
-          <Link
-            className="grid grid-rows-2 justify-items-center gap-2"
-            to={routes.explore()}
-          >
+          <Link className="" to={routes.explore()}>
             <MdAddCircleOutline className="text-2xl" />
-            Create
           </Link>
         </li>
         <li>
-          <Link
-            className="grid grid-rows-2 justify-items-center gap-2"
-            to={routes.explore()}
-          >
+          <Link className="" to={routes.explore()}>
             <MdPersonOutline className="text-2xl" />
-            Me
           </Link>
         </li>
         <li>
-          <Link
-            className="grid grid-rows-2 justify-items-center gap-2"
-            to={routes.explore()}
-          >
+          <Link className="" to={routes.explore()}>
             <MdOutlineSettings className="text-2xl" />
-            Settings
           </Link>
         </li>
       </ul>
