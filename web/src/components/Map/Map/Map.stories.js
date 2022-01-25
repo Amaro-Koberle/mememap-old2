f0bc766ae202dcc9327a76437dc9721299d58af0
Amaro-Koberle <amaro@amarokoberle.com>
2022-01-25 16:14:11 +0100

    added stories to the map component

--- web/src/components/Map/Map/Map.stories.js
+++ web/src/components/Map/Map/Map.stories.js
@@ -1,7 +1,20 @@
 import Map from './Map'
 
+const graphData = {
+  nodes: [
+    { id: 'a', name: 'A', color: '#FF0000' },
+    { id: 'b', name: 'B', color: '#00FF00' },
+    { id: 'c', name: 'C', color: '#0000FF' },
+  ],
+  links: [
+    { id: 'ab', source: 'a', target: 'b', name: 'AB', color: '#FF0000' },
+    { id: 'bc', source: 'b', target: 'c', name: 'BC', color: '#00FF00' },
+    { id: 'ca', source: 'c', target: 'a', name: 'CA', color: '#0000FF' },
+  ],
+}
+
 export const generated = () => {
-  return <Map />
+  return <Map graphData={graphData} />
 }
 
 export default { title: 'Components/Map' }
