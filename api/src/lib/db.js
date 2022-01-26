01596b9852342a842b30154c8137cf09ad39ccff
Amaro-Koberle <amaro@amarokoberle.com>
2022-01-26 20:41:22 +0100

    added deleted field to Node model and soft delete middleware

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -14,6 +14,36 @@ export const db = new PrismaClient({
   log: emitLogLevels(['info', 'warn', 'error']),
 })
 
+async function main() {
+  /***********************************/
+  /* SOFT DELETE MIDDLEWARE */
+  /***********************************/
+
+  db.$use(async (params, next) => {
+    // Check incoming query type
+    if (params.model == 'Post') {
+      if (params.action == 'delete') {
+        // Delete queries
+        // Change action to an update
+        params.action = 'update'
+        params.args['data'] = { deleted: true }
+      }
+      if (params.action == 'deleteMany') {
+        // Delete many queries
+        params.action = 'updateMany'
+        if (params.args.data != undefined) {
+          params.args.data['deleted'] = true
+        } else {
+          params.args['data'] = { deleted: true }
+        }
+      }
+    }
+    return next(params)
+  })
+}
+
+main()
+
 handlePrismaLogging({
   db,
   logger,
